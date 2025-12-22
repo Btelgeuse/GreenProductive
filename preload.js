3eeef455fd46dbd2fb41d0d045f5bbdd67d527c3
Erkhembileg Ariunbold <a.erkhembileg9@gmail.com>
2025-12-21 16:31:28 -0800

    connected the binary script to the front-end & printed the text and partials on the console.

--- preload.js
+++ preload.js
@@ -2,4 +2,23 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
   loadPage: (page) => ipcRenderer.send("load-page", page),
+
+  // Speech Service
+  startSpeechService: () => ipcRenderer.invoke("start-speech-service"),
+  stopSpeechService: () => ipcRenderer.invoke("stop-speech-service"),
+
+  onSpeechFinal: (callback) => {
+    ipcRenderer.removeAllListeners("speech-final");
+    ipcRenderer.on("speech-final", (_, text) => callback(text));
+  },
+
+  onSpeechPartial: (callback) => {
+    ipcRenderer.removeAllListeners("speech-partial");
+    ipcRenderer.on("speech-partial", (_, text) => callback(text));
+  },
+
+  removeAllSpeechListeners: () => {
+    ipcRenderer.removeAllListeners("speech-final");
+    ipcRenderer.removeAllListeners("speech-partial");
+  },
 });
